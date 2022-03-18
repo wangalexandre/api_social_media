52caddaa0e0ed1bb402edf1c10095c5f4b995280
Alexandre Wang <darksailor@hotmail.fr>
2022-03-18 11:51:38 +0800

    query parameters + some code refactoring

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,7 +4,7 @@ from .. import schemas, oauth2
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from typing import List
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/posts",
@@ -28,11 +28,18 @@ while True:
 # get all posts stored in db
 # current user is defined as int type but doesn't really matter as it will return a dictionary either way
 @router.get('/', response_model=List[schemas.Post])
-def get_posts(current_user: int = Depends(oauth2.get_current_user)):
-    print(current_user['email'])
+def get_posts(current_user: int = Depends(oauth2.get_current_user), 
+                limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    # print(current_user['email'])
     # cursor.execute allows you to use raw SQL
-    cursor.execute("""SELECT p.*, u.id, u.email, u.created_at FROM public.posts p JOIN public.users u ON p.user_id = u.id """)
-    
+    cursor.execute("""
+                    SELECT p.*, u.id, u.email, u.created_at 
+                    FROM public.posts p 
+                    JOIN public.users u ON p.user_id = u.id
+                    WHERE (%s IS NULL OR p.title LIKE %s)
+                    LIMIT %s OFFSET %s
+                    """, (search, f'%{search}%', limit, skip))
+
     # use cursor.fetchall() if retrieving multiple items
     posts = cursor.fetchall()
     
@@ -44,16 +51,27 @@ def get_posts(current_user: int = Depends(oauth2.get_current_user)):
 def create_posts(post: schemas.PostCreate, current_user: int = Depends(oauth2.get_current_user)):
     print(current_user['id'])
     # using %s placeholders instead of {post.title} directly in the SQL code to prevent SQL injection attacks. %s sanitizes the variable
-    cursor.execute("""INSERT INTO posts (title, content, published, user_id) VALUES (%s, %s, %s, %s) RETURNING * """, 
-    (post.title, post.content, post.published, current_user['id']))
+    cursor.execute("""
+                        INSERT INTO posts (title, content, published, user_id) 
+                        VALUES (%s, %s, %s, %s) RETURNING * """, 
+                        (post.title, post.content, post.published, current_user['id']))
     
     # use cursor.fetchone() to retrieve only the created post
     new_post = cursor.fetchone()
+    print(new_post['id'])
     
+    cursor.execute("""
+                    SELECT p.*, u.id, u.email, u.created_at 
+                    FROM public.posts p 
+                    JOIN public.users u ON p.user_id = u.id
+                    WHERE p.id = %s
+                    """, (new_post['id'], ))
+    new_post_with_id = cursor.fetchone()
+
     # need to commit to push changes to database, everything above is staging the changes
     conn.commit()
 
-    return new_post
+    return new_post_with_id
 
 
 # retrieve a specific post based on id - path parameter
@@ -61,7 +79,11 @@ def create_posts(post: schemas.PostCreate, current_user: int = Depends(oauth2.ge
 # specifying that the id input should be an integer to validate user input
 def get_post(id: int, current_user: int = Depends(oauth2.get_current_user)):
     # need to convert id to str to make sure SQL query interprets it, cannot interpret integer
-    cursor.execute("""SELECT p.*, u.id, u.email, u.created_at FROM public.posts p JOIN public.users u ON p.user_id = u.id WHERE p.id = %s """, (str(id), ))
+    cursor.execute("""
+                        SELECT p.*, u.id, u.email, u.created_at 
+                        FROM public.posts p JOIN public.users u 
+                        ON p.user_id = u.id 
+                        WHERE p.id = %s """, (str(id), ))
     post = cursor.fetchone()
     
     if not post:

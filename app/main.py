ccda85acd8b1c41dbb23e56feb4a19b5292d3d63
Alexandre Wang <darksailor@hotmail.fr>
2022-03-26 11:38:56 +0800

    adding CORS policy in main.py + comprehensive package reqs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from .routers import post, user, auth, vote
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 print(settings.database_hostname)
 
 app = FastAPI()
+
+# create an allow list for sources allowed to interact with API, for example 'https://www.google.com' for Google domain
+# can use '*' to make a public API to allow access from everyone
+origins = ['*']
+
+# CORS policy to control access from external sources
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    # specify specific path operations allowed
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # in FastAPI, this is a Path Operation - similar to route in Flask
